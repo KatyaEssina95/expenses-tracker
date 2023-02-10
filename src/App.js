9abc3c9ab427df4034e80ca002740b9058779e69
Katya Essina <ekaterina.essina@fundingcircle.com>
2023-02-10 17:34:07 +0000

    Create basic ExpenseItem

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const expenses = [
+    {title: "Car Insurance", amount: 124.23, date: new Date(2022, 2, 28)},
+    {title: "Groceries", amount: 224.23, date: new Date(2022, 2, 28)}
+  ];
 
+  return (
+    <div>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
+    </div>
+  )
+}
 export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+import './ExpenseItem.css'
+
+function ExpenseItem(props) {
+  // TODO: add styling - tailwind?
+
+  return (
+    <div className="expense-item">
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+      </div>
+      <div className="expense-item__price">{props.price}</div>
+    </div>
+  )
+}
+
+export default ExpenseItem;
